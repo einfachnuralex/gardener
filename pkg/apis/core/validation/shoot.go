d9920dea43a26501382c43f946ed960711d8d45e
Manuel Ganter <manuel.ganter@hotmail.de>
2020-09-10 13:41:48 +0200

    fixed bug where you could not apply seed when not defining feature gates in shoot.spec.networking

--- pkg/apis/core/validation/shoot.go
+++ pkg/apis/core/validation/shoot.go
@@ -606,13 +606,16 @@ func validateNetworking(networking core.Networking, fldPath *field.Path) field.E
 		allErrs = append(allErrs, field.Required(fldPath.Child("type"), "networking type must be provided"))
 	}
 
-	fmt.Println("FeatureGateDS: " + strconv.FormatBool(*networking.FeatureGates.IPv6DualStack))
+	dualstack := false
+	if networking.FeatureGates != nil && networking.FeatureGates.IPv6DualStack != nil {
+		dualstack = *networking.FeatureGates.IPv6DualStack
+	}
 
 	if networking.Nodes != nil {
 		path := fldPath.Child("nodes")
 
 		// validate for dual-stack
-		if *networking.FeatureGates.IPv6DualStack {
+		if dualstack {
 			res, err := net.IsDualStackCIDRStrings(strings.Split(*networking.Nodes, ","))
 			if err != nil || !res {
 				allErrs = append(allErrs, field.Invalid(path, networking.Nodes, "when IPv6DualStack enabled, you have to define ipv4 and ipv6"))
@@ -627,7 +630,7 @@ func validateNetworking(networking core.Networking, fldPath *field.Path) field.E
 	if networking.Pods != nil {
 		path := fldPath.Child("pods")
 		// validate for dual-stack
-		if *networking.FeatureGates.IPv6DualStack {
+		if dualstack {
 			res, err := net.IsDualStackCIDRStrings(strings.Split(*networking.Pods, ","))
 			if err != nil || !res {
 				allErrs = append(allErrs, field.Invalid(path, networking.Pods, "when IPv6DualStack enabled, you have to define ipv4 and ipv6"))
@@ -643,7 +646,7 @@ func validateNetworking(networking core.Networking, fldPath *field.Path) field.E
 	if networking.Services != nil {
 		path := fldPath.Child("services")
 		// validate for dual-stack
-		if *networking.FeatureGates.IPv6DualStack {
+		if dualstack {
 			res, err := net.IsDualStackCIDRStrings(strings.Split(*networking.Services, ","))
 			if err != nil || !res {
 				allErrs = append(allErrs, field.Invalid(path, networking.Services, "when IPv6DualStack enabled, you have to define ipv4 and ipv6"))
